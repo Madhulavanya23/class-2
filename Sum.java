fc6c3fd1ae3e1b429e35acc30ae66c9dfbaf899e
Madhulavanya23 <madhulavanyalavanya621@gmail.com>
2026-02-07 14:31:23 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,15 @@
+public class Sum {
+    public static void main(String[] args) {
+     //assignmentoperators
+        int a=10,b=5;
+        int c=20;
+        //arthemetic opertors
+        System.out.println(a+b);
+        if(c>0&&c%2==0)
+            System.out.println("positive number and even number");
+System.out.println(a++);
+System.out.println(a);
+
+    }
+    
+}
