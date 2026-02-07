8a11e0aa968bb9aa32d36214240485e6e736d797
Madhulavanya23 <madhulavanyalavanya621@gmail.com>
2026-02-07 11:06:32 +0530

    Add files via upload

---
+++ Score.java
@@ -0,0 +1,12 @@
+public class Score {
+    public static void main(String args[]){
+    int maths=56;
+    int english=60;
+    int physics=60;
+    int total=maths+english+physics;
+    int avg=total/3;
+    System.out.println(total);
+    System.out.println(avg);
+    }
+    
+}
