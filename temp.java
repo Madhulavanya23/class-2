c86d9f40a7596795c79ee2a9c59b57f18a05cf0a
Madhulavanya23 <madhulavanyalavanya621@gmail.com>
2026-02-07 15:22:37 +0530

    Add files via upload

---
+++ temp.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class temp {
+    public static void main(String[] args) {
+        int a=5,b=6;
+        int t=a;
+         a=b;
+         b=t;
+         System.out.println("after swaping a= "+a);
+          System.out.println("after swaping b= "+b);
+        
+        
+    }
+    
+}
