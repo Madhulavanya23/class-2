d5fca4358f9a9e808b5b347c7db787a3c98b52ab
Madhulavanya23 <madhulavanyalavanya621@gmail.com>
2026-02-07 14:22:22 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,17 @@
+public class operators {
+    public static void main(String[] args) {
+        int a=5,b=6;
+        System.out.println("arthemetic operators");
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a%b);
+        System.out.println("unary operators");
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a);
+        System.out.println(--a);
+        System.out.println(a--);
+    }
+    
+}
