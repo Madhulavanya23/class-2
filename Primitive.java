a9f4db9906b3bacd2e7e71b03c428ae3824644d0
Madhulavanya23 <madhulavanyalavanya621@gmail.com>
2026-02-07 10:52:50 +0530

    Add files via upload

---
+++ Primitive.java
@@ -0,0 +1,18 @@
+public class Primitive {
+    public static void main(String[] args) {
+        byte b=121;
+        short s=159;
+        int i=234;
+        long l=2345;
+        float f=23.45f;
+        double d=2345.23455678;
+        char  c='m';
+        boolean ba=true;
+        System.out.println(d);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(~4);
+    }
+    
+}
